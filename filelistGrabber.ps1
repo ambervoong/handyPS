c1f768e4fe29b45f5fd1124dfa1558697ea9db51
amber <44959941+ambervoong@users.noreply.github.com>
2023-07-04 16:53:30 +0800

    Add files via upload

---
+++ filelistGrabber.ps1
@@ -0,0 +1,11 @@
+﻿# Use param -Path to specify the path to grab
+param ($Path = "C:\")
+
+$pcname = hostname
+
+# Script to get the file name, length, and timestamp of all files in a folder
+gci $Path -Force -Recurse | ForEach-Object {
+                                                if ($_.GetType().Name -eq "FileInfo") {
+                                                    $_ | Select Name, Length, LastWriteTimeUtc | Export-Csv -NoTypeInformation -Path "($pcname)-fileList.csv"                                   
+                                                    }
+                                            }
